46484ef5a081ca5c0e7501060f638effe490c50c
Satoshi Hasegawa <satoshi@fnal.gov>
2016-09-21 10:48:55 -0500

    Customized to FNALLPC

--- crabconfig_draft.py
+++ crabconfig_draft.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = "privateMCProduction"
+config.General.requestName = "privateMCProduction#REQUESTDATE#"
 config.General.workArea = 'crab_privateMCProduction'
 config.General.transferLogs = True
 
@@ -19,10 +19,11 @@ config.Data.unitsPerJob = 1200
 config.Data.totalUnits = #NUMBEREVENTS#
 config.Data.publication = True
 config.Data.outputDatasetTag = 'eventLHEGEN'
+config.Data.outLFNDirBase = '/store/group/lpctthrun2/'
 
 config.section_("Site")
-config.Site.storageSite = 'T2_DE_DESY'
+config.Site.storageSite = 'T3_US_FNALLPC'
 
 config.section_("User")
-config.User.voGroup = "dcms"
+# config.User.voGroup = "dcms"
 
