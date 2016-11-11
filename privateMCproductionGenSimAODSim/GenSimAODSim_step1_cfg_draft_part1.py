47a8ac065a3db6fdc715d3d362521b39080276c0
MarcoA <marco.harrendorf@partner.kit.edu>
2016-11-11 20:03:38 +0100

    Added GenSimAODSim_step1 config, Config is now fully updated for Moriond2017

--- privateMCproductionGenSimAODSim/GenSimAODSim_step1_cfg_draft_part1.py
+++ privateMCproductionGenSimAODSim/GenSimAODSim_step1_cfg_draft_part1.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: step1 --filein /store/XX --fileout file:GenSimAODSim_step1.root --pileup_input dbs:/Neutrino_E-10_gun/RunIISpring15PrePremix-PU2016_80X_mcRun2_asymptotic_v14-v2/GEN-SIM-DIGI-RAW --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 80X_mcRun2_asymptotic_v14 --step DIGIPREMIX_S2,DATAMIX,L1,DIGI2RAW,HLT:25ns10e33_v2 --nThreads 4 --datamix PreMix --era Run2_2016 --python_filename GenSimAODSim_step1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 960
+# with command line options: step1 --filein /store/XX --fileout file:GenSimAODSim_step1.root --pileup_input dbs:/Neutrino_E-10_gun/RunIISpring15PrePremix-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v2-v2/GEN-SIM-DIGI-RAW --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 --step DIGIPREMIX_S2,DATAMIX,L1,DIGI2RAW,HLT:@frozen2016 --nThreads 4 --datamix PreMix --era Run2_2016 --python_filename EXO-RunIISummer16DR80Premix-00004_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 82
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.StandardSequences.Eras import eras
@@ -22,7 +22,7 @@ process.load('SimGeneral.MixingModule.digi_MixPreMix_cfi')
 process.load('Configuration.StandardSequences.DataMixerPreMix_cff')
 process.load('Configuration.StandardSequences.SimL1EmulatorDM_cff')
 process.load('Configuration.StandardSequences.DigiToRawDM_cff')
-process.load('HLTrigger.Configuration.HLT_25ns10e33_v2_cff')
+process.load('HLTrigger.Configuration.HLT_25ns15e33_v4_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
