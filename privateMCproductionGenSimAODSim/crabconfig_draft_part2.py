40c2526539f87f5416cd60cde0270023b09f179f
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2016-11-29 11:43:08 +0100

    Removed T3_Beijing settings

--- privateMCproductionGenSimAODSim/crabconfig_draft_part2.py
+++ privateMCproductionGenSimAODSim/crabconfig_draft_part2.py
@@ -1,11 +1,11 @@
  ]
 
 config.section_("Site")
-#config.Site.storageSite = 'T2_DE_DESY'
-#config.Site.whitelist = ['T2_DE_DESY']
+config.Site.storageSite = 'T2_DE_DESY'
+config.Site.whitelist = ['T2_DE_DESY']
 ## T3 Beijing
-config.Site.storageSite = 'T2_CN_Beijing'
-config.Site.whitelist = ['T2_*']
+#config.Site.storageSite = 'T2_CN_Beijing'
+#config.Site.whitelist = ['T2_*']
 ## T3 Beijing
 
 config.section_("User")
