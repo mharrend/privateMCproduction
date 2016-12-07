8f066779a450e4191fd8aa8bb22a869714c86f7c
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2016-12-07 17:16:40 +0100

    Added FNAL storage

--- privateMCproductionGenSimAODSim/crabconfig_draft_part2.py
+++ privateMCproductionGenSimAODSim/crabconfig_draft_part2.py
@@ -2,14 +2,6 @@
 # ]
 
 config.section_("Site")
-#config.Site.storageSite = 'T2_DE_DESY'
-#config.Site.whitelist = ['T2_DE_DESY']
-## T3 Beijing
-config.Site.storageSite = 'T2_CN_Beijing'
-config.Site.whitelist = ['T2_*']
-## T3 Beijing
+config.Site.storageSite = 'T3_US_FNALLPC'
 
-config.section_("User")
-## Only german users
-#config.User.voGroup = "dcms"
 
